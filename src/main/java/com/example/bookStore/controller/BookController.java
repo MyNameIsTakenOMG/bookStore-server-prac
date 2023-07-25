d29304f0ac0c7e0ba163395871d10f6e54b0ff4b
Sean Fang <fangzhengonly@gmail.com>
2023-07-25 17:22:38 -0400

    add prod application yml file and setup postgresql db for prod env

--- src/main/java/com/example/bookStore/controller/BookController.java
+++ src/main/java/com/example/bookStore/controller/BookController.java
@@ -2,6 +2,13 @@ package com.example.bookStore.controller;
 
 import com.example.bookStore.dto.BookDTO;
 import com.example.bookStore.service.BookService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +22,15 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+//    @Operation(summary = "get a list of books")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200",description = "successfully retrieved books",
+//                    content = {@Content(
+//                            mediaType = "application/json",
+//                            array = @ArraySchema(schema = @Schema(implementation = BookDTO.class)))}),
+//            @ApiResponse(responseCode = "401", description = "access denied"),
+//            @ApiResponse(responseCode = "404", description = "not found")
+//    })
     @GetMapping("api/v1/books")
     public ResponseEntity<List<BookDTO>> getBooks(){
         List<BookDTO> books = bookService.getBooks();
