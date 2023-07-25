8fe2d3ad18e2cc5291810ba9dfccbe88ed1591cc
Sean Fang <fangzhengonly@gmail.com>
2023-07-25 13:18:25 -0400

    replace insert statement with select statement(avoid duplicate primary key) in data.sql to make maven test pass

--- src/test/resources/insertInitialBookDataForTest.sql
+++ src/test/resources/insertInitialBookDataForTest.sql
@@ -1,1 +1,1 @@
-insert into book(id,title,description, release_year) values('b5607d38-8fc1-43ef-b44e-34967083c80c','test title','The japanese secret to long and happy life',2018)
+insert into book(id,title,description, release_year) values('b5607d38-8fc1-43ef-b44e-34967083c79a','test title','The japanese secret to long and happy life',2018)
